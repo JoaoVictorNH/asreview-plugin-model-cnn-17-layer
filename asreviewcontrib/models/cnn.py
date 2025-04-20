3a1d89e64d0e4e367aaee866fc8bc66d7c7b748f
João Victor do N. Holanda <61544856+JoaoVictorNH@users.noreply.github.com>
2025-04-20 13:16:06 -0300

    Update cnn.py

--- asreviewcontrib/models/cnn.py
+++ asreviewcontrib/models/cnn.py
@@ -19,7 +19,7 @@ from tensorflow.keras import Sequential
 from tensorflow.keras import layers
 from tensorflow.keras import activations
 from tensorflow.keras import optimizers
-from keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier  # Fixed import path
 from tensorflow.keras import callbacks
 from tensorflow.keras import backend
 
